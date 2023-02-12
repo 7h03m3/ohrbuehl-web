64f7d810f0c489e460b93d36f3fb7a1465ec5f38
Thomas Stanger <stangth1@students.zhaw.ch>
2023-02-12 09:01:11 +0100

    feat(frontend): show welcome and full organization name in shooting range manager

---
+++ frontend/src/app/public/public-welcome/public-welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'public-public-welcome',
+  templateUrl: './public-welcome.component.html',
+  styleUrls: ['./public-welcome.component.css'],
+})
+export class PublicWelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

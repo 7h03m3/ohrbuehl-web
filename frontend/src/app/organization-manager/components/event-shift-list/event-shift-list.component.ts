5b56f45e366e2bc703173965db3225ad29d1d663
Thomas Stanger <stangth1@students.zhaw.ch>
2023-03-24 17:27:24 +0100

    feat(frontend): add event category filter to organization manager view

--- frontend/src/app/organization-manager/components/event-shift-list/event-shift-list.component.ts
+++ frontend/src/app/organization-manager/components/event-shift-list/event-shift-list.component.ts
@@ -9,6 +9,8 @@ import { EventShiftDto } from '../../../shared/dtos/event-shift.dto';
 import { EventShiftListItemDto } from './dtos/event-shift-list-item.dto';
 import { EventStaffPoolApi } from '../../../api/classes/event-staff-pool-api';
 import { AuthService } from '../../../auth/auth.service';
+import { EventCategoryDto } from '../../../shared/dtos/event-category.dto';
+import { EventCategoryApi } from '../../../api/classes/event-category-api';
 
 @Component({
   selector: 'app-event-shift-list',
@@ -16,11 +18,14 @@ import { AuthService } from '../../../auth/auth.service';
   styleUrls: ['./event-shift-list.component.css'],
 })
 export class EventShiftListComponent {
-  eventList = new Array<EventShiftListItemDto>();
-  displayedColumns: string[] = ['time', 'day', 'title', 'category', 'shift', 'rangeOfficer', 'pool', 'action'];
+  public eventList = new Array<EventShiftListItemDto>();
+  public displayedColumns: string[] = ['time', 'day', 'title', 'category', 'shift', 'rangeOfficer', 'pool', 'action'];
+  public categoryList = new Array<EventCategoryDto>();
+  public selectedCategory = 0;
   private organizationId = 0;
   private eventApi: EventApi;
   private poolApi: EventStaffPoolApi;
+  private categoryApi: EventCategoryApi;
 
   constructor(
     private apiService: ApiService,
@@ -31,6 +36,7 @@ export class EventShiftListComponent {
   ) {
     this.eventApi = this.apiService.getEvent();
     this.poolApi = this.apiService.getStaffPool();
+    this.categoryApi = this.apiService.getEventCategory();
   }
 
   private static isAssigned(shift: EventShiftDto): boolean {
@@ -43,7 +49,21 @@ export class EventShiftListComponent {
   }
 
   public ngOnInit(): void {
+    this.selectedCategory = this.userLocalData.getEventCategory();
     this.organizationId = this.authService.getManagingOrganizationId();
+
+    this.categoryApi.getAll().subscribe((response) => {
+      this.categoryList = new Array<EventCategoryDto>();
+
+      const allCategory = new EventCategoryDto();
+      allCategory.name = 'Alle';
+      this.categoryList.push(allCategory);
+
+      response.forEach((category) => {
+        this.categoryList.push(category);
+      });
+    });
+
     this.fetch();
   }
 
@@ -71,36 +91,44 @@ export class EventShiftListComponent {
     return element.totalShifts != 0;
   }
 
+  public onCategoryChange(categoryId: number) {
+    this.userLocalData.setEventCategory(categoryId);
+    this.selectedCategory = categoryId;
+    this.fetch();
+  }
+
   private fetch() {
     this.eventApi.getAllWithShiftsByOrganizationId(this.organizationId).subscribe((response) => {
       this.eventList = new Array<EventShiftListItemDto>();
       response.forEach((event) => {
-        const element = new EventShiftListItemDto();
-        element.event = event;
+        if (this.selectedCategory == 0 || event.categoryId == this.selectedCategory) {
+          const element = new EventShiftListItemDto();
+          element.event = event;
 
-        if (event.shifts != undefined) {
-          element.totalShifts = event.shifts.length;
-          const rangeOfficerShiftList = event.shifts.filter((value) => {
-            return value.category.requiresRangeOfficer;
-          });
-          const assignedRangeOfficerShiftList = event.shifts.filter((value) => {
-            return value.category.requiresRangeOfficer && value.assignedStaffId != null;
-          });
-          element.totalRangeOfficer = rangeOfficerShiftList.length;
-          element.assignedRangeOfficer = assignedRangeOfficerShiftList.length;
+          if (event.shifts != undefined) {
+            element.totalShifts = event.shifts.length;
+            const rangeOfficerShiftList = event.shifts.filter((value) => {
+              return value.category.requiresRangeOfficer;
+            });
+            const assignedRangeOfficerShiftList = event.shifts.filter((value) => {
+              return value.category.requiresRangeOfficer && value.assignedStaffId != null;
+            });
+            element.totalRangeOfficer = rangeOfficerShiftList.length;
+            element.assignedRangeOfficer = assignedRangeOfficerShiftList.length;
 
-          event.shifts.forEach((shift) => {
-            if (EventShiftListComponent.isAssigned(shift)) {
-              element.assignedShifts = element.assignedShifts + 1;
-            }
+            event.shifts.forEach((shift) => {
+              if (EventShiftListComponent.isAssigned(shift)) {
+                element.assignedShifts = element.assignedShifts + 1;
+              }
+            });
+          }
+
+          this.poolApi.getAllByOrganizationAndEvent(this.organizationId, event.id).subscribe((poolList) => {
+            element.totalInPool = poolList.length;
           });
+
+          this.eventList.push(element);
         }
-
-        this.poolApi.getAllByOrganizationAndEvent(this.organizationId, event.id).subscribe((poolList) => {
-          element.totalInPool = poolList.length;
-        });
-
-        this.eventList.push(element);
       });
     });
   }

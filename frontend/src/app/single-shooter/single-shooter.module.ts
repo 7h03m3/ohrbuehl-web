e25d695a2944f3918b8d53234bf41b925534c0c7
Thomas Stanger <stangth1@students.zhaw.ch>
2024-02-20 21:15:04 +0100

    fix(frontend): resolve import issues

--- frontend/src/app/single-shooter/single-shooter.module.ts
+++ frontend/src/app/single-shooter/single-shooter.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleShooterComponent } from './single-shooter.component';
-import { MaterialModule } from '../shared/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { BusinessHoursModule } from '../business-hours/business-hours.module';
 import { SingleShooterRoutingModule } from './single-shooter-routing.module';
 import { SingleShooterInformationComponent } from './components/single-shooter-information/single-shooter-information.component';
 import { SingleShooterReservationListComponent } from './components/single-shooter-reservation-list/single-shooter-reservation-list.component';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [SingleShooterComponent, SingleShooterInformationComponent, SingleShooterReservationListComponent],
-  imports: [CommonModule, MaterialModule, SharedModule, SingleShooterRoutingModule, BusinessHoursModule],
+  imports: [CommonModule, SharedModule, SingleShooterRoutingModule, BusinessHoursModule, MaterialModule],
 })
 export class SingleShooterModule {}

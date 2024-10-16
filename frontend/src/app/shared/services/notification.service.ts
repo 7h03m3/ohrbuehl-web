321a99e1649959aebfd62d0c9dc68d73f397bd7a
Thomas Stanger <stangth1@students.zhaw.ch>
2024-10-16 17:03:46 +0200

    feat: update docker compose files

--- frontend/src/app/shared/services/notification.service.ts
+++ frontend/src/app/shared/services/notification.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 import { ContactMessageStatus } from '../enums/contact-message-status.enum';
 import { AuthService } from '../../auth/auth.service';
 import { ContactMessageApi } from '../../api/contact-message-api';
+import { ApplicationAdminApiService } from '../../api/application-admin-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
   private messageCount = 0;
+  private applicationCount = 0;
 
-  constructor(public authService: AuthService, private contactMessageApi: ContactMessageApi) {}
+  constructor(
+    public authService: AuthService,
+    private contactMessageApi: ContactMessageApi,
+    private applicationService: ApplicationAdminApiService,
+  ) {}
 
   public update() {
     if (this.authService.isShootingRangeManager()) {
@@ -17,9 +23,19 @@ export class NotificationService {
         this.messageCount = +response;
       });
     }
+
+    if (this.authService.isAdmin()) {
+      this.applicationService.getCount().subscribe((response) => {
+        this.applicationCount = +response;
+      });
+    }
   }
 
   public getMessageCount(): number {
     return this.messageCount;
+  }
+
+  public getApplicationCount(): number {
+    return this.applicationCount;
   }
 }
